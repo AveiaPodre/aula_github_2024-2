e56c682f9728feaeae77a403e35e2a19c3bc5403
Raffael <raffael13fernandes@gmail.com>
2024-10-06 19:05:28 -0300

    adicionando log de depósito e saque

--- conta.py
+++ conta.py
@@ -1,8 +1,10 @@
 class Conta:
+
     def init(self, cpf, titular, saldo=0):
         self.cpf = cpf
         self.titular = titular
         self.saldo = saldo
+        self.transacoes = []
         
     def consultar_saldo(self):
         return self.saldo
@@ -10,6 +12,7 @@ class Conta:
     def depositar(self, valor):
         if valor > 0:
             self.saldo += valor
+            self.log_transacao("depósito", valor)
             print('Depósito realizado com sucesso')
         else:
             print('Valor inválido para depósito')
@@ -17,6 +20,7 @@ class Conta:
     def sacar(self, valor):
         if valor > 0 and valor <= self.saldo:
             self.saldo -= valor
+            self.log_transacao("saque", valor)
             return True
         else:
             return False
@@ -24,6 +28,9 @@ class Conta:
     def transferir(self, conta_destino, valor):
         #Todo: Implementar
         return
+    
+    def log_transacao(self, tipo, valor):
+        self.transacoes.append({'tipo': tipo, 'valor': valor, 'saldo': self.saldo})
     
     
     
